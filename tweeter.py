9d128d71451f262e9ec41772bd5939e21e1120bf
WillNess210 <WillNess210@gmail.com>
2018-10-06 23:21:47 -0600

    added function for sending

--- tweeter.py
+++ tweeter.py
@@ -1,18 +1,25 @@
 import tweepy
 import readlines
-#getting from text file
-result = readlines.getAllLinesIn('twitterid.txt')
-#twitter application credentials
-consumer_key=result[0]
-consumer_secret=result[1]
 
-#twitter user credentials
-access_token=result[2]
-access_token_secret=result[3]
+def sendTweet(message):
+    #getting from text file
+    result = readlines.getAllLinesIn('twitterid.txt')
+    #twitter application credentials
+    consumer_key=result[0]
+    consumer_secret=result[1]
+    #twitter user credentials
+    access_token=result[2]
+    access_token_secret=result[3]
+    #setting up API
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    tweepyapi = tweepy.API(auth)
+    #tweeting
+    try:
+        if tweepyapi.update_status(message):
+            print("Sent tweet: " + message)
+    except tweepy.error.TweepError as e:
+        print("Error sending tweet: " + e)
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-tweepyapi = tweepy.API(auth)
-
-tweepyapi.update_status('Hello World!! Again')
+if __name__ == "__main__":
+    sendTweet("Testing a tweet!")
